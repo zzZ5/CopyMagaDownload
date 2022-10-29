8ba6ae633b8e2b08c9293efcfbcfb283616af750
zzZ5 <baoju_liu@foxmail.com>
2022-10-29 12:30:09 +0800

    优化

--- copymaga.py
+++ copymaga.py
@@ -6,7 +6,11 @@ import time
 import requests
 
 url_root = 'https://copymanga.site'
-
+scroll_time = 300
+proxies = {
+    "http": "http://127.0.0.1:7890",
+    "https": "http://127.0.0.1:7890"
+}
 
 def download_comic(comic_name, comic_path):
     chapter_list = get_comic_chapter(comic_name, comic_path)
@@ -20,10 +24,12 @@ def download_comic(comic_name, comic_path):
 def get_comic_chapter(comic_name, comic_path):
 
     print("获取漫画{}章节".format(comic_name))
-    driver = webdriver.Chrome(executable_path="chromedriver")
     options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--ignore-ssl-errors')
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(executable_path="chromedriver", options=options)
     driver.get(comic_path)
     time.sleep(2)
     for i in range(1, 3):
@@ -59,23 +65,45 @@ def get_comic_chapter(comic_name, comic_path):
 
     print("获取漫画{}章节成功！".format(comic_name))
 
-    return chapter_list
+    choose = input('是否下载所有章节？（y/n）')
+    if choose == 'y':
+        return chapter_list
+    else:
+        i = 0
+        for chapter in chapter_list:
+            i += 1
+            print(str(i) + " " + chapter[0])
+        choose = input('请输入你要下载章节的序号, 用空格隔开？（例如: 1-5 9-20 25 28）')
+        choose_slice = choose.split(" ")
+        choose_chapter_list = []
+        for sli in choose_slice:
+            if '-' in sli:
+                a, b = sli.split('-')
+                a = int(a) - 1
+                b = int(b) - 1
+                choose_chapter_list.extend(chapter_list[a:b])
+            else:
+                choose_chapter_list.append(chapter[int(sli)])
+        return choose_chapter_list
 
 
 def get_chapter_piclist(comic_name, chapter_name, chapter_url):
     print("获取漫画{} {}图片地址".format(comic_name, chapter_name))
-    driver = webdriver.Chrome(executable_path="chromedriver")
     options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--ignore-ssl-errors')
+    # options.add_argument('--headless')
+    # options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(executable_path="chromedriver", options=options)
     driver.get(chapter_url)
     time.sleep(3)
     scroll = 100
-    for i in range(1, 400):
+    for i in range(1, scroll_time):
         scroll += 250
         js = "var q=document.documentElement.scrollTop={}".format(scroll)
         driver.execute_script(js)
-        time.sleep(0.1)
+        time.sleep(0.15)
+    time.sleep(3)
     chapter_html_url = "temp/_{}/_{}.html".format(
         comic_name, chapter_name)
 
@@ -104,7 +132,7 @@ def download_piclist(comic_name, chapter_name, pic_list):
     j = 0
     for i in pic_list:
         j += 1
-        pic = requests.get(i).content
+        pic = requests.get(i, proxies=proxies).content
 
         pic_path = download_folder + '/' + os.path.split(i)[-1]
 
@@ -126,7 +154,7 @@ while(1):
               }
     url = "https://copymanga.site/api/kb/web/searchs/comics"
 
-    response = requests.request("GET", url, params=params)
+    response = requests.request("GET", url, params=params, proxies=proxies)
 
     comic_list = response.json()['results']['list']
 
@@ -135,18 +163,16 @@ while(1):
         index += 1
         print(str(index) + ' ' + i['name'] + ': ' + i['alias'])
 
-    res = input("是否要下载漫画？（y/n）")
-    if res == 'y':
-        index = int(input("请输入你要下载漫画的序号："))
-    else:
-        res = input("是否要退出下载？（y/n）")
-        if res == 'y':
+    choose = input("下载哪个漫画？（请输入下载漫画的序号）")
+    try:
+        index = int(choose) - 1
+    except:
+        choose = input("是否要退出下载？（y/n）")
+        if choose == 'y':
             print('再见')
             break
         else:
             continue
-
-    index -= 1
     comic_name = comic_list[index]['name']
     comic_path_word = comic_list[index]['path_word']
     comic_path = "https://copymanga.site/comic/{}".format(comic_path_word)
